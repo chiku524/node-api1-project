6d3c5b23a974aaf483bf6ca7a3df0f32f9ad05bd
Nico Chikuji <chiku524@icloud.com>
2020-09-03 21:02:42 -0400

    got the post to workm finished mvp

--- server.js
+++ server.js
@@ -35,11 +35,11 @@ server.listen(port, () => {
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
 
-    function validateBody(body) {
-        return true;
-    }
+    // function validateBody(body) {
+    //     return true;
+    // }
 
-    if(validateBody(body)) {
+    if(userInfo) {
         userInfo.id = shortid.generate();
         users.push(userInfo);
         res.status(201).json(userInfo);
@@ -53,7 +53,38 @@ server.get('/api/users', (req, res) => {
 })
 
 server.get('/api/users/:id', (req, res) => {
-    id = req.params.id;
+    const {id} = req.params;
+    const userById = users.filter(user => user.id === id);
 
+    if(userById[0]) {
+        res.send(userById)
+    } else {
+        res.status(404).json({message: "user not found, try a different id"})
+    }
+})
 
+server.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const found = users.find(user => user.id === id);
+
+    if(found) {
+        users = users.filter(user => user.id !== id);
+    } else {
+        res.status(404).json({message: "user not found"});
+    }
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
+
+    let index = users.findIndex(user => user.id === id);
+
+    if(index !== -1) {
+        changes.id = id;
+        users[index] = changes;
+        res.status(200).json(users[index]);
+    } else {
+        res.status(404).json({message: "user not found"});
+    }
 })
